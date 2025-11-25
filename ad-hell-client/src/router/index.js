25843cf357901ffb711f2276220ea174fcb090aa
zkvn1103@naver.com <zkvn1103@naver.com>
2025-11-25 17:03:48 +0900

    feat : admin button, change sidebar

--- ad-hell-client/src/router/index.js
+++ ad-hell-client/src/router/index.js
@@ -129,6 +129,12 @@ const routes = [
                 component: () => import('@/pages/advertise/AdMain.vue'),
             },
 
+            {
+              path: 'admin/home',
+              name: 'AdminHome',
+              component: () => import('@/pages/admin/Home.vue'),
+            },
+
             // 어드민 신고 목록 페이지
             {
               path: 'admin/reports',
