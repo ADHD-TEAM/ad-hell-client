745b01ead8d143ff495cdac79d75303e82cf2b38
okok02110211 <okok02110211@gmail.com>
2025-11-25 12:11:31 +0900

    광고 생성 디테일 카테고리 모달, 메인페이지

--- ad-hell-client/src/pages/advertise/AdMain.vue
+++ ad-hell-client/src/pages/advertise/AdMain.vue
@@ -5,14 +5,26 @@ import IconSrc from '@/assets/icon/icon-movie.svg'
 </script>
 
 <template>
-  <PageTitle
-      title="광고 목록"
-      :show-category="false"
-      :icon-src= "IconSrc"
-  />
-  <AdSlotList/>
+  <div class="ad-main-page">
+    <PageTitle
+        class="page-title-margin"
+        title="광고 목록"
+        :show-category="false"
+        :icon-src="IconSrc"
+    />
+    <AdSlotList />
+  </div>
 </template>
 
 <style scoped>
+/* 전체 페이지 래퍼 (원하면 여기서도 패딩/마진 조절 가능) */
+.ad-main-page {
+  /* 필요하면 여기에 기본 레이아웃 옵션 추가 */
+}
 
+/* 제목을 위/왼쪽에서 50px 떨어뜨리기 */
+.page-title-margin {
+  margin-top: 75px;
+  margin-left: 75px;
+}
 </style>
